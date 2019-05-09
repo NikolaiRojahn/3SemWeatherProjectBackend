1df4c5d4ed272cd2f0fd6a8ac331fbd39c3b98c8
Porselicious <porseg69@gmail.com>
2019-05-09 12:03:24 +0200

    server deploy credentials added

--- src/test/java/accepttests/AcceptTestDemo.java
+++ src/test/java/accepttests/AcceptTestDemo.java
@@ -23,7 +23,7 @@ public class AcceptTestDemo {
 
   @BeforeClass
   public static void setUpBeforeAll() throws LifecycleException, ServletException, MalformedURLException {
-     RestAssured.baseURI = "https://deploy.mydemos.dk/";
+     RestAssured.baseURI = "https://www.joergoertel.com/";
     //RestAssured.port = 80;
     //RestAssured.basePath = APP_CONTEXT;
     RestAssured.defaultParser = Parser.JSON;
